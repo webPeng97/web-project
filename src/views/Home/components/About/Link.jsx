709e6cc197164a1bc13470f988a243094cd8e60a
webPeng <2571879534@qq.com>
2019-08-21 18:19:56 +0800

    hahaha

--- src/views/Home/components/About/Link.jsx
+++ src/views/Home/components/About/Link.jsx
@@ -32,6 +32,26 @@ class Link extends Component {
             </div>
           </div>
         </div>
+        <div className="about-link">
+          <div className="about-link-name xiaozhan">
+            <span className="text">xiaozhan</span>
+          </div>
+          <div className="about-link-title">你这个问题充钱就能解决</div>
+          <div className="about-link-go">
+            <div className="github">
+              <Icon type="github" />
+            </div>
+            <div className="weibo">
+              <Icon type="weibo-circle" />
+            </div>
+            <div className="chrome">
+              <Icon type="chrome" />
+            </div>
+            <div className="instagram">
+              <Icon type="instagram" />
+            </div>
+          </div>
+        </div>
       </Fragment>
     )
   }
